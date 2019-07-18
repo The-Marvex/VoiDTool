6d060019f074c5733196f640983097171d4e05bb
Manas Awasthi <marvex17@gmail.com>
2019-07-19 00:14:50 +0530

    Update VoIDTool.java

--- VoIDTool.java
+++ VoIDTool.java
@@ -10,24 +10,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VoIDTool {
-    private final File Db;
     private PrintStream out;
-    private SimpleGdb Gdb;
-    public VoIDTool(File f1) throws IDMapperException {
-        this(f1, System.out);
+    private final File oldDb;
+    private final File newDb;
+    private SimpleGdb oldGdb;
+    private SimpleGdb newGdb;
+    FileWriter file = new FileWriter(".\\rdf.void", true);
+
+    public VoIDTool(File f1, File f2) throws IDMapperException, IOException {
+        this(f1,f2, System.out);
     }
-    public VoIDTool(File f1, OutputStream out){
-        Db = f1;
+    public VoIDTool(File f1, File f2, OutputStream out) throws IOException {
+        oldDb= f1;
+        newDb= f2;
         this.out = new PrintStream(out);
     }
     Map<DataSource, Integer> Set = new HashMap<DataSource, Integer>();
 
     public void initDatabases() throws IDMapperException{
-        String url = "jdbc:derby:jar:(" + Db + ")database";
-        Gdb = SimpleGdbFactory.createInstance("Database", url);
+        String url = "jdbc:derby:jar:(" + oldDb+ ")database";
+        oldGdb = SimpleGdbFactory.createInstance("db1", url);
+        String url2 = "jdbc:derby:jar:(" + newDb + ")database";
+        newGdb = SimpleGdbFactory.createInstance("db2", url2);
+    }
+    public void run()throws IDMapperException{
+        initDatabases();
+        createVoid();
+
     }
     public void createVoid(String db1, String db2)throws IOException{
-        FileWriter file = new FileWriter(".\\rdf.void", true);
+        db1 = db1.toLowerCase();
+        db2 = db2.toLowerCase();
         file.write("@prefix void:  <http://rdfs.org/ns/void#> .\n");
         file.write("@prefix pav:   <http://purl.org/pav/> .\n");
         file.write("@prefix freq:  <http://purl.org/cld/freq/> .\n");
@@ -46,14 +59,14 @@ public class VoIDTool {
         file.write("@prefix foaf:  <http://xmlns.com/foaf/0.1/> .\n");
         file.write("\n");
         switch(db1) {
-            case "GMPL":
+            case "gmpl":
                 file.write("<http://rdf.wikipathways.org/20190713/rdf/gpml> \n");
                 file.write("        a                 dcat:Distribution ;\n");
                 file.write("void:dataDump     <https://jenkins.bigcat.unimaas.nl/job/GPML%20to%20GPML%20+%20WP%20RDF/ws/WP2RDF/output/gpml/*zip*/gpml.zip> ;\n");
                 file.write("dcat:downloadURL  <https://jenkins.bigcat.unimaas.nl/job/GPML%20to%20GPML%20+%20WP%20RDF/ws/WP2RDF/output/gpml/*zip*/gpml.zip> ;");
 
                 break;
-            case "WikiPathways":
+            case "wikipathways":
                 file.write("http://rdf.wikipathways.org/20190713/datasetDescription/");
                 file.write("        a                           void:Dataset ;\n");
                 file.write(" void:DatasetDescription ;\n");
@@ -69,12 +82,26 @@ public class VoIDTool {
                 file.write(" void:objectsTarget   <https://www.wikidata.org/entity/Q2013> ; \n");
                 file.write(" void:subjectsTarget  <http://rdf.wikipathways.org/20190713/rdf/>. \n");
                 break;
-            case "HMDB":
+            case "hmdb":
                 file.write();
                 break;
-            case "Chebi":
-                file.write();
+            case "chebi":
+                file.write("void:DatasetDescription ; \n");
+                file.write("dcterms:title \"Chemical Entities of Biological Interest (ChEBI)\"@en ; \n");
+                file.write("dcterms:description \"Chemical Entities of Biological Interest (ChEBI) is a freely available dictionary of molecular entities focused on &#39;small&#39; chemical compounds.\"@en ; \n");
+                file.write("foaf:homepage <http://www.ebi.ac.uk/chebi/> ; \n");
+                file.write("void:dataDump <ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.owl> ; \n");
+                file.write("dcterms:license <http://creativecommons.org/licenses/by-sa/3.0/> ; \n");
                 break;
+            case "chembl":
+                file.write("void:DatasetDescription ; \n");
+                file.write("dcterms:title \"The ChEMBL Database\" ;\n");
+                file.write("dcterms:description \\\"ChEMBL is a database of bioactive drug-like small molecules, it contains 2-D structures, calculated properties (e.g. logP, Molecular Weight, Lipinski Parameters, etc.) and abstracted bioactivities (e.g. binding constants, pharmacology and ADMET data). The data is abstracted and curated from the primary scientific literature, and cover a significant fraction of the SAR and discovery of modern drugs.\\\" ;\");\n");
+                file.write("foaf:page <ftp://ftp.ebi.ac.uk/pub/databases/chembl/> ;");
+                file.write("dcterms:license <http://creativecommons.org/licenses/by-sa/3.0/> ;");
+                file.write("void:dataDump <ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBL-RDF/20.0/chembl_20.0_targetrel.ttl.gz> .");
+            case "uniprot":
+                file.write();
         }
     }
 }
